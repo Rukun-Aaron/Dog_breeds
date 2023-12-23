02ad3a786415c4ae7a5593b6c6e037fa8096ae62
rukun aaron <raar518@aucklanduni.ac.nz>
2023-12-24 01:02:18 +1300

    react yay

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import  FileUpload  from './FileUpload';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Your React App</h1>
+      <FileUpload />
+  
+    </div>
+  );
+}
+
+export default App;
