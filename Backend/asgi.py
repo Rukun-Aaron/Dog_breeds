ea8c0bf31d3520d3bc667ba72532cddbee54fa34
rukun aaron <raar518@aucklanduni.ac.nz>
2024-01-13 00:50:39 +1300

    added stuff

--- Backend/asgi.py
+++
@@ -1,7 +0,0 @@
-"""App entry point."""
-import uvicorn
-from library import create_app
-app = create_app()
-
-if __name__ == "__main__":
-    uvicorn.run("asgi:app", reload=True, port=6789, log_config="log.ini")
