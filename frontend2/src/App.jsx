1e68e8bc51e9f0206d470f8a5e58e4fd392ed7fd
rukun aaron <raar518@aucklanduni.ac.nz>
2024-01-15 01:37:26 +1300

    added daisy ui

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -6,6 +6,7 @@ import ImageUploadNew from './components/ImageUploadNew';
 function App() {
   return (
     <div className="w-full h-screen drawer drawer-end">
+      <Navbar/>
       <div className="h-full w-full dark:bg-neutral-900 scrollbar overflow-y-auto">
         <ImageUploadNew />
       </div>
