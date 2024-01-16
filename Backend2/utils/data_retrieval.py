aecb0f0964dbe24bf67cebbc21f6b33d24040330
rukun aaron <raar518@aucklanduni.ac.nz>
2024-01-17 02:07:30 +1300

    finished it yay

--- Backend2/utils/data_retrieval.py
+++ Backend2/utils/data_retrieval.py
@@ -4,6 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+labels = [
+    "id", "image_link", "good_with_children", "good_with_other_dogs",
+    "shedding", "grooming", "drooling", "coat_length",
+    "good_with_strangers", "playfulness", "protectiveness",
+    "trainability", "energy", "barking", "min_life_expectancy",
+    "max_life_expectancy", "max_height_male", "max_height_female",
+    "max_weight_male", "max_weight_female", "min_height_male",
+    "min_height_female", "min_weight_male", "min_weight_female",
+    "name"
+]
 db_config = {
     'host': os.getenv('DB_HOST'),
     'port': os.getenv('DB_PORT'),
@@ -23,8 +33,9 @@ def get_breed_info(breed):
         """
         cursor.execute(select_query)
         
-        breed_info = cursor.fetchone()
-        
+        data = cursor.fetchone()
+        breed_info = dict(zip(labels, data))
+
         if breed_info is None:
             return (f"No dog_breeds named {breed} found in the database.")
         else:
