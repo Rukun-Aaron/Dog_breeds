e34269250779d81d970921492a55c3af6b3bc965
hlu750 <hlu750@aucklanduni.ac.nz>
2024-01-20 20:28:46 +1300

    Temporary navbar

--- frontend2/src/components/NavBar.jsx
+++ frontend2/src/components/NavBar.jsx
@@ -18,17 +18,24 @@ function Navbar() {
   return (
     <div className={`navbar z-10 left-1/2 -translate-x-1/2 w-11/12 max-w-4xl xl:max-w-5xl top-4 shadow dark:shadow-xl dark:bg-neutral-800 backdrop-blur-sm rounded-xl  ${isHomePage ? 'absolute' : 'fixed '}`}>
       <div className="flex-1 flex gap-2">
-        <div className="cursor-pointer" onClick={() => navigate('/')}>
-          <p className="text-lg">
-            Logo, Name
+        <div className="cursor-pointer flex items-center justify-center" onClick={() => navigate('/')}>
+        <div className="pl-4 pr-3">
+          <img
+            alt="logo"
+            src="/logo2.ico"
+            className="h-10 w-10"
+          />
+        </div>
+          <p className="text-2xl text-white font-semibold">
+            DogIO
           </p>
         </div>
       </div>
-      <div className="md:flex gap-2 hidden">
+      <div className="gap-8 text-lg text-white font-semibold">
         <button onClick={() => navigate('/')}>
           Home
         </button>
-        <button onClick={() => navigate('/detection')}>
+        <button className="pr-6" onClick={() => navigate('/detection')}>
           Detect
         </button>
       </div>
