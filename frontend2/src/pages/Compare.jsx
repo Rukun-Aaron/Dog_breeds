756b5570659f6d69e7ebb73832f1f57a1e9fc0cd
rukun aaron <raar518@aucklanduni.ac.nz>
2024-01-30 02:13:16 +1300

    compare stuff

--- frontend2/src/pages/Compare.jsx
+++ frontend2/src/pages/Compare.jsx
@@ -1,10 +1,42 @@
-import React, { useState, useEffect } from 'react'
+import  { useState, useEffect } from 'react'
+import * as React from 'react';
+
 import Navbar from '../components/NavBar'
 import { getBreedInfoImage } from '../services/apiService';
+
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
 const Compare = () => {
   const [breeds, setBreeds] = useState([])
+  const [filter, setFilter] = useState('')
+  
+  const filteredBreeds = breeds.filter((breed) => {
+    return breed.name.toLowerCase().includes(filter.toLowerCase())
+  })
+  const tags = [
+    { title: 'Trainable' },
+    { title: 'Energetic' },
+    { title: 'Less Tendancy to Bark' },
+    { title: 'Good with Strangers' },
+    { title: 'Good with Kids' },
+    { title: 'Good with Other Dogs' },
+    { title: 'Less Tendancy to Shed' },
+    { title: 'Little Grooming Needs' },
+    { title: 'Tendancy to Droom' },
+    { title: 'Long Coat Length' },
+    { title: 'Short Coat Length' },
+    { title: 'Playful' },
+    { title: 'Protective' }
+  ]
 
-  console.log(breeds)
+  console.log(filteredBreeds);
   useEffect(() => {
     // Fetch breed data when the component mounts
     const fetchData = async () => {
@@ -24,36 +56,81 @@ const Compare = () => {
     fetchData();
   }, []);
   return (
-    <div className='w-full h-full flex flex-col items-center relative z-0' >
+    <div className='w-full h-screen drawer drawer-end' >
       <div className="pt-4"><Navbar />
-        {breeds.length > 0 && (
-          <div className='mt-4 w-full flex flex-col gap2 my-8 px-8'>
-            {breeds.map((breed, index) => (
-              <div
-                key={index}
-                className="flex w-full items-center p-4 gap-4 rounded-xl 
+
+        <div className='h-full w-full dark:bg-neutral-900 scrollbar overflow-y-auto'>
+          <div className='flex justify-center overflow-x-hidden pt-24 pb-4 h-fit w-full'>
+            <div className='max-w-5xl xl:max-w-6xl w-11/12'>
+              <div className='sm:px-2 pb-2 flex items-center justify-between'>
+                <div className='join-vertical'>
+                  <input
+                    placeholder="Filter"
+                    className="dark:bg-neutral-900 dark:text-neutral-100 dark:border-white 
+                    input input-bordered join-item w-full mb-5"
+                    onChange={(e) => {
+                      e.preventDefault();
+                      setFilter(e.target.value);
+                      // setCurrentPage(1);
+                    }}
+                  />
+                  <Autocomplete
+                    multiple
+                    id="checkboxes-tags-demo"
+                    options={tags}
+                    disableCloseOnSelect
+                    getOptionLabel={(option) => option.title}
+                    renderOption={(props, option, { selected }) => (
+                      <li {...props}>
+                        <Checkbox
+                          icon={icon}
+                          checkedIcon={checkedIcon}
+                          style={{ marginRight: 8 }}
+                          checked={selected}
+                        />
+                        {option.title}
+                      </li>
+                    )}
+                    style={{ width: 500 }}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Checkboxes" placeholder="Favorites" />
+                    )}
+                  />
+                </div>
+              </div>
+              <div className='w-full'>
+                {filteredBreeds.length > 0 && (
+                  <div className='mt-4 w-full flex flex-col gap2 my-8 px-8'>
+                    {filteredBreeds.map((breed, index) => (
+                      <div
+                        key={index}
+                        className="flex w-full items-center p-4 gap-4 rounded-xl 
               transition-all cursor-pointer hover:bg-neutral-100 dark:hover:bg-neutral-800 
               animate-ease-in-out animate-jump animate-once animate-duration-700 relative"
-              >
-                <div className="w-32 h-16 flex items-center justify-center">
-                  <img
-                    src={breed.image} 
-                    alt={`Selected ${index + 1}`}
-                    className="max-w-32 max-h-16 rounded-md"
-                  />
+                      >
+                        <div className="w-32 h-16 flex items-center justify-center">
+                          <img
+                            src={breed.image}
+                            alt={`Selected ${index + 1}`}
+                            className="max-w-32 max-h-16 rounded-md"
+                          />
 
-                </div>
-                <div className="dark:text-neutral-100 truncate mr-auto">
-                  {breed.name}
-                </div>
+                        </div>
+                        <div className="dark:text-neutral-100 truncate mr-auto">
+                          {breed.name}
+                        </div>
 
 
+                      </div>
+                    ))}
+
+                  </div>
+
+                )}
               </div>
-            ))}
-
+            </div>
           </div>
-
-        )}
+        </div>
 
       </div>
     </div>
